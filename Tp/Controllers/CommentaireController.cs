212309f1323bc7f55de3e8671c86845cd93535f0
RichardLacroix <richardlacroix@videotron.ca>
2015-01-17 18:46:15 -0500

    Creation des dossier view et controleur
    Creation des dossier view et controleur

---
+++ Tp/Controllers/CommentaireController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tp.Controllers
+{
+    public class CommentaireController : Controller
+    {
+        //
+        // GET: /Commentaire/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
